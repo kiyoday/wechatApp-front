a96c091dd4a37dd2b0df4689ef7c0628d8230b1c
kiyoday <1260583285@qq.com>
2020-05-30 00:23:18 +0800

    基类
    定义基类并请求基类回调

--- pages/home/home-model.js
+++ pages/home/home-model.js
@@ -1,19 +1,29 @@
 
-class Home {
+import {Base} from '../utils/base.js';
 
+class Home extends Base{
   constructor(){
-
+    super();
   }
 
   getBannerData(id, callBack){
-    wx.request({
-      url:'http://s.com/api/v1/banner/' + id,
-      method:'GET',
-      success:function(res){
-        // console.log(res);
-        callBack(res);
+    var params = {
+      url:'banner/'+id,
+      sCallBack:function(res){
+        callBack && callBack(res.items);
       }
-    })
+    }
+
+    this.request(params);
+
+    // wx.request({
+    //   url:'http://s.com/api/v1/banner/' + id,
+    //   method:'GET',
+    //   success:function(res){
+    //     // console.log(res);
+    //     callBack(res);
+    //   }
+    // })
   }
 }
 
---
+++ pages/utils/config.js
@@ -0,0 +1,9 @@
+
+class Config{
+
+
+}
+
+Config.restUrl = 'http://s.com/api/v1/';
+
+export {Config};
