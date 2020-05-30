6d0ef0220e07f831fcddd235e086775095829e01
kiyoday <1260583285@qq.com>
2020-05-30 20:33:43 +0800

    主题分类

--- pages/theme/theme.js
+++ pages/theme/theme.js
@@ -1,66 +1,35 @@
-// pages/theme/theme.js
+import { Theme } from 'theme-model.js';
+var theme = new Theme(); //实例化  主题列表对象
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
+    
+  },
+
+  onReady:function(){
+    wx.setNavigationBarTitle({
+      title: this.data.titleName
+  });
+  },
+
+  onLoad: function (option) {
+    this.data.titleName=option.name;
+    this.data.id=option.id;
+    this._loadData();
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  _loadData:function(callback){
+    var that = this;
+    /*获取单品列表信息*/
+    theme.getProductorData(this.data.id,(data) => {
+        that.setData({
+            themeInfo: data,
+            loadingHidden:true
+        });
+        callback && callback();
+    });
   }
+  
 })
